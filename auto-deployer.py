f557e088139e9b96981129501803454293e6fa95
Hessam Ghassemi <44605128+hqasemi@users.noreply.github.com>
2021-09-11 02:16:06 +0430

    Update auto-deployer.py

--- auto-deployer.py
+++ auto-deployer.py
@@ -1,7 +1,12 @@
+import argparse
+import sys
+import yml
+from host import HOST
+
+
 def main ():
-    global args,args_dict
+    global args
     args = parse_flags()
-    args_dict = vars(args)
 
     ordered_args_list = get_args_by_order()
 
@@ -9,31 +14,29 @@ def main ():
 
     host_objects = create_host_objects(hosts)
     
-    run_all_commands(ordered_args_list,host_objects)
+    run_all_commands(ordered_args_list, host_objects)
     
 def parse_flags():
     """
     parse_flags parses input flags
     """
-    import argparse
     parser = argparse.ArgumentParser(
-        description='This script would help deployment of repeatedly tasks on remote machines for example run commands and send or receive files using ssh and scp respectively')
+        description='This script would help deployment of repeatedly tasks on remote machines for example run commands and send or receive files using ssh and scp respectively'
+    )
     
     #parser.add_argument('--host', nargs='+', help='added new host', dest='hosts',type=str, required=False)
 
-    parser.add_argument('--command', dest='command',action='append', help="Run a command on all remote machines",required=True,type=str)
-    parser.add_argument('--hosts', dest='hosts', help="Path of hosts YAML config file that contains ip's and passwords",required=True,type=str)
-    parser.add_argument('--upload', dest='upload',action='append', help="Upload a file to a remote machine",required=False,type=str)
-    parser.add_argument('--download', dest='download',action='append', help="Download a file from a remote machine",required=False,type=str)
+    parser.add_argument('--command', dest='command',action='append', help="Run a command on all remote machines", required=True, type=str)
+    parser.add_argument('--hosts', dest='hosts', help="Path of hosts YAML config file that contains ip's and passwords", required=True, type=str)
+    parser.add_argument('--upload', dest='upload',action='append', help="Upload a file to a remote machine", required=False, type=str)
+    parser.add_argument('--download', dest='download',action='append', help="Download a file from a remote machine", required=False, type=str)
 
     return parser.parse_args()
 
 def get_args_by_order():
-    import sys
     return [arg[2:] for arg in sys.argv if arg.startswith("--")]
 
 def load_hosts_yaml_file():
-    import yaml
     try:
         return load_yaml(args.hosts)
     except yaml.parser.ParserError as e:
@@ -41,18 +44,21 @@ def load_hosts_yaml_file():
         exit(1)
 
 def load_yaml(yaml_file_path):
-    import yaml
     with open(yaml_file_path, 'r') as cfg:
         return yaml.load(cfg)
         
 def create_host_objects(hosts):
     host_objects = []
+    
     for host in hosts:
-        from host import HOST
-
+        host_name = host["name"]
+        host_ip = host["ip"]
+        user = host["user"]
+        password = host["password"]
+        
         try:
-            h = HOST(host["name"],host["ip"],host["user"],host["password"])
-            host_objects.append(h)
+            host_obj = HOST(host_name, host_ip, user, password)
+            host_objects.append(host_obj)
         except KeyError as e:
             print(f"Error: maybe there is a problem is syntax of config file {args.hosts}: {e} does not exists in {host}")
         except Exception as e:
@@ -60,11 +66,14 @@ def create_host_objects(hosts):
     return host_objects
 
 def run_all_commands(ordered_args_list,host_objects):
+    args_dict = vars(args)
+
     for arg in ordered_args_list:
-        if isinstance(args_dict[arg],str):
+        if isinstance(args_dict[arg], str):
             continue
+            
         command = args_dict[arg].pop(0)
-        # print(args_dict)
+
         for host in host_objects:
             try:
                 output = host.run(arg,command)
